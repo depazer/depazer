44eb2e594ebf11e92ded67f4a64c470071b6ce55
liangzhensheng <1250564179@qq.com>
2023-07-31 16:38:23 +0800

    feat: init docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "dependency-analyzer",
+  description: "A CLI tool for analyzing package dependencies",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/package-dependency-analyze/dependency-analyzer' }
+    ]
+  }
+})
