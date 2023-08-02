b4e49df20f38a4318b39fa4306c4f3d8a3e75486
liangzhensheng <1250564179@qq.com>
2023-08-02 13:16:49 +0800

    fix(docs): add docs base property

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/dependency-analyzer/',
   title: "dependency-analyzer",
   description: "A CLI tool for analyzing package dependencies",
   themeConfig: {
