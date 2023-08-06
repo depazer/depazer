b1cd8ab0800da5df9fc366e52c9131060895810b
Jon-a-than <2140145186@qq.com>
2023-08-06 14:09:15 +0800

    feat(api): add npm registry API

--- packages/web/src/api/npmRegistry.ts
+++ packages/web/src/api/npmRegistry.ts
@@ -1,29 +1,38 @@
 import { get, set } from 'idb-keyval'
 import { useFetch } from '@vueuse/core'
 
-export async function ApiGetNpmPackageInfo(name: string, version: string, baseURL: string) {
+/**
+ * @desc 获取npm包信息, 从缓存中获取, 如果没有则从网络获取, 未指定具体版本则不缓存
+ * @param name 包名
+ * @param version 版本 可为 '' 查询所有版本 ‘latest’ 查询最新版本
+ * @param baseURL npm registry 接口地址
+ */
+export async function ApiGetNpmPackageInfo(
+  name: string,
+  version: string,
+  baseURL: string
+): Promise<{
+  data: any
+  error: any
+}> {
   const url = `${baseURL || 'https://registry.npmjs.org/'}${name}/${version}`
 
-  const cached = await get(url)
+  const cached = version === '' ? undefined : await get(url)
 
   if (cached) {
-    return { data: cached }
+    return { data: cached, error: undefined }
   }
 
   const { data, error } = await useFetch(
     url,
-    {
-      headers: { 'Content-Type': 'application/json' }
-    },
+    { headers: { 'Content-Type': 'application/json' }, method: 'GET' },
     {
       afterFetch({ data }) {
-        set(url, data)
+        if (version !== '') set(url, data)
         return { data }
       }
     }
-  )
-    .get()
-    .json()
+  ).json()
 
-  return error.value ? { error: error.value } : { data: data.value }
+  return { error: error.value, data: data.value }
 }
--- packages/web/src/views/ModuleNet/components/PackageInfo.vue
+++ packages/web/src/views/ModuleNet/components/PackageInfo.vue
@@ -2,8 +2,7 @@
 import { usePackageStore } from '@/stores/package'
 import { useAppStore } from '@/stores/app'
 import { storeToRefs } from 'pinia'
-import { computed, onMounted, ref, watchEffect } from 'vue'
-import { useFetch } from '@vueuse/core'
+import { computed, ref, watchEffect } from 'vue'
 import { ApiGetNpmPackageInfo } from '@/api/npmRegistry'
 
 const appStore = useAppStore()
@@ -12,63 +11,37 @@ const packageStore = usePackageStore()
 const { currentPackage } = storeToRefs(packageStore)
 
 const emit = defineEmits<{ close: [] }>()
+const fetching = ref<boolean>(false)
 
-const packageRegistryURL = computed(() => {
-  return `${currentRegistry.value}${currentPackage.value.name}/${currentPackage.value.version}`
+watchEffect(async () => {
+  fetching.value = true
+  const { data } = await ApiGetNpmPackageInfo(
+    currentPackage.value.name,
+    currentPackage.value.version,
+    currentRegistry.value
+  )
+  packageInfo.value = data
+  setTimeout(() => (fetching.value = false), 300)
 })
 
-const packageInfo = ref({
-  name: '@vue/runtime-dom',
-  version: '3.3.4',
-  description: '@vue/runtime-dom',
-  sideEffects: false,
-  repository: {
-    type: 'git',
-    url: 'git+https://github.com/vuejs/core.git',
-    directory: 'packages/runtime-dom'
-  },
-  keywords: ['vue'],
-  author: {
-    name: 'Evan You'
-  },
-  license: 'MIT',
-  bugs: {
-    url: 'https://github.com/vuejs/core/issues'
-  },
-  homepage: 'https://github.com/vuejs/core/tree/main/packages/runtime-dom#readme',
-  dependencies: {
-    '@vue/shared': '3.3.4',
-    '@vue/runtime-core': '3.3.4',
-    csstype: '^3.1.1'
-  },
-  _id: '@vue/runtime-dom@3.3.4',
-  dist: {
-    fileCount: 12,
-    unpackedSize: 979547,
-    size: 251403
-  },
-  _npmUser: {
-    name: 'yyx990803',
-    email: 'yyx990803@gmail.com'
-  },
-  directories: {},
-  maintainers: [
-    {
-      name: 'yyx990803',
-      email: 'yyx990803@gmail.com'
-    }
-  ],
-  _hasShrinkwrap: false,
-  _cnpmcore_publish_time: '2023-05-18T08:31:25.817Z',
-  publish_time: 1684398685817,
-  _source_registry_name: 'default'
-})
+const packageInfo = ref()
 
-// const { data } = await ApiGetNpmPackageInfo(
-//   currentPackage.value.name,
-//   currentPackage.value.version,
-//   currentRegistry.value
-// )
+const formattedPackageInfo = computed(() => {
+  return {
+    description: packageInfo.value?.description ?? '无',
+    dist: {
+      size: packageInfo.value?.dist?.size
+        ? (packageInfo.value.dist.size / 1024).toFixed(2) + 'KB'
+        : 'UNKNOWN',
+      unpackedSize: packageInfo.value?.dist?.unpackedSize
+        ? (packageInfo.value.dist.unpackedSize / 1024).toFixed(2) + 'KB'
+        : 'UNKNOWN'
+    },
+    license: packageInfo.value?.license ?? 'UNKNOWN',
+    keywords: packageInfo.value?.keywords ?? [],
+    dependencies: Object.entries(packageInfo.value?.dependencies ?? {})
+  }
+})
 </script>
 
 <template>
@@ -79,17 +52,17 @@ const packageInfo = ref({
   >
     <header flex="~ justify-between items-center">
       <div>
-        <span class="px-1 rounded-l dark:bg-blue-6 bg-indigo-3 text-indigo-8 dark:text-blue-2">{{
-          currentPackage.name
-        }}</span>
-        <span class="px-1 rounded-r bg-lime-2 dark:bg-lime-6 text-lime-6 dark:text-lime-1">{{
-          currentPackage.version
-        }}</span>
+        <code class="px-1 rounded-l dark:bg-blue-6 bg-indigo-3 text-indigo-8 dark:text-blue-2">
+          {{ currentPackage.name }}
+        </code>
+        <code class="px-1 rounded-r bg-lime-2 dark:bg-lime-6 text-lime-6 dark:text-lime-1">
+          {{ currentPackage.version }}
+        </code>
       </div>
       <button
         type="button"
         title="关闭"
-        class="pa-1 ma-0 border-none rounded-md"
+        class="rounded-md pa-1 ma-0 border-none"
         bg="transparent hover:gray-2 hover:dark:slate-8"
         @click="emit('close')"
       >
@@ -97,9 +70,58 @@ const packageInfo = ref({
       </button>
     </header>
 
-    <p class="select-none">
-      <span class="after:content-[':'] mr-1">开源协议</span>
-      <code class="px-1 rounded bg-red-1 text-red-6">{{ packageInfo.license }}</code>
-    </p>
+    <section v-show="!fetching">
+      <div class="my-4">
+        <span class="font-bold select-none">概述</span>
+        <p class="my-2">{{ formattedPackageInfo.description }}</p>
+      </div>
+      <p class="select-none">
+        <span class="font-bold mr-2">大小</span>
+        <code>{{ formattedPackageInfo.dist.size }}</code>
+        <span class="font-bold mx-2">解压大小</span>
+        <code>{{ formattedPackageInfo.dist.unpackedSize }}</code>
+      </p>
+      <p class="select-none">
+        <span class="mr-2 font-bold">开源协议</span>
+        <code class="px-1 rounded-sm bg-red-1 text-red-6 dark:text-red-3 dark:bg-red-1/30">
+          {{ packageInfo?.license }}
+        </code>
+      </p>
+      <div v-show="packageInfo?.keywords?.length" class="select-none my-4">
+        <span class="mr-2 font-bold">关键字</span>
+        <div flex="~ wrap items-start" class="gap-1 mt-2">
+          <code
+            class="px-1 rounded-sm text-cyan-6 bg-cyan-1 dark:text-cyan-3 dark:bg-cyan-1/30"
+            v-for="key of packageInfo?.keywords ?? []"
+            :key="key"
+          >
+            {{ key }}
+          </code>
+        </div>
+      </div>
+      <div
+        v-show="Object.keys(packageInfo?.dependencies ?? {}).length !== 0"
+        flex="~ wrap col items-start"
+        class="gap-1 my-4"
+      >
+        <span class="font-bold select-none">生产依赖</span>
+        <li
+          class="px-1"
+          v-for="[dependency, version] of Object.entries(packageInfo?.dependencies ?? {})"
+          :key="dependency + version"
+        >
+          {{ dependency }}@{{ version }}
+        </li>
+      </div>
+    </section>
+    <!-- 骨架屏 -->
+    <section v-show="fetching" class="animate-pulse py-4">
+      <span class="mr-2 inline-block bg-gray h-4 rounded w-1/4"></span>
+      <span class="inline-block bg-gray h-4 rounded w-1/3"></span>
+      <p class="bg-gray h-4 rounded"></p>
+      <p class="bg-gray h-4 rounded w-3/4"></p>
+      <p class="bg-gray h-4 rounded"></p>
+      <p class="bg-gray h-4 rounded"></p>
+    </section>
   </div>
 </template>
