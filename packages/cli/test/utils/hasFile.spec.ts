686a7f06c1e82d2f8b053cc981220644fb45b46f
liangzhensheng <1250564179@qq.com>
2023-08-11 16:33:43 +0800

    test(hasFolder): add hasFolder test case

--- packages/cli/test/utils/hasFile.spec.ts
+++ packages/cli/test/utils/hasFile.spec.ts
@@ -1,4 +1,4 @@
-import { hasFile } from '@/utils/hasFile'
+import { hasFile, hasFolder } from '@/utils/hasFile'
 import { describe, test } from 'vitest'
 
 describe.concurrent('hasFile', () => {
@@ -9,6 +9,16 @@ describe.concurrent('hasFile', () => {
 
   test('should return false if file does not exist', async ({ expect }) => {
     const result = await hasFile('package.js')
+    expect(result).toBe(false)
+  })
+
+  test('should return true if folder exists', async ({ expect }) => {
+    const result = await hasFolder('src')
+    expect(result).toBe(true)
+  })
+
+  test('should return false if folder does not exist', async ({ expect }) => {
+    const result = await hasFolder('sc')
     expect(result).toBe(false)
   })
 })
