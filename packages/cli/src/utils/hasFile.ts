1535b9c94d1c828fd3acb588cfcc61d910e329d2
liangzhensheng <1250564179@qq.com>
2023-08-11 16:18:16 +0800

    feat(cli): improve scanner method

--- packages/cli/src/utils/hasFile.ts
+++ packages/cli/src/utils/hasFile.ts
@@ -1,4 +1,5 @@
 import { resolve } from 'node:path'
+import { existsSync } from 'fs'
 import { stat } from 'node:fs/promises'
 
 export async function hasFile(...filePath: string[]) {
@@ -8,4 +9,8 @@ export async function hasFile(...filePath: string[]) {
   } catch (error) {
     return false
   }
+}
+
+export function hasFolder(filePath: string) {
+  return existsSync(filePath)
 }
