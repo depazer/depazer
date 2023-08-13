89988ec294707d0121d6b5cf75dd0b84e5e01b07
Jon-a-than <2140145186@qq.com>
2023-08-13 16:02:26 +0800

    build(web): use unplugin-vue-i18n replace vue-cli-plugin-i18n

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,14 +4,16 @@ import { defineConfig } from 'vite'
 import unocss from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import AutoImport from 'unplugin-auto-import/vite'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite'
+import autoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     unocss(),
     vue(),
     vueJsx(),
-    AutoImport({
+    vueI18n({ jitCompilation: true }),
+    autoImport({
       imports: ['vue', 'vue-router', '@vueuse/core', 'pinia'],
       vueTemplate: true,
       dts: 'src/types/auto-imports.d.ts'
@@ -19,9 +21,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      find: 'vue-i18n',
-      replacement: 'vue-i18n/dist/vue-i18n.cjs.js'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
