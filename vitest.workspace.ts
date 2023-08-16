3d6922e4cc5323ed757855b4280017a1ebebb8e1
Jon-a-than <2140145186@qq.com>
2023-08-16 16:01:35 +0800

    chore(vitest): update vitest workspace config

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,9 @@
 import { defineWorkspace } from 'vitest/config'
 
-export default defineWorkspace(['packages/*'])
+export default defineWorkspace([
+  'packages/core',
+  'packages/cli',
+  'packages/web',
+  'packages/shared',
+  'packages/vite'
+])
