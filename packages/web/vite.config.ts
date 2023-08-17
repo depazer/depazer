12f1ad3c940d7cf1e5fcfce0153baea11f7809b3
Jon-a-than <2140145186@qq.com>
2023-08-17 17:36:44 +0800

    chore(web): web set api proxy

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -23,5 +23,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4936'
+      }
+    }
   }
 })
