346598ef9a6e87aae6d4b5e14bf791692773861c
liangzhensheng <1250564179@qq.com>
2023-08-20 11:17:41 +0800

    fix(cli): startAnalyzer add environmentScanner check

--- packages/cli/src/commands/analyze/actions/startAnalyzer.ts
+++ packages/cli/src/commands/analyze/actions/startAnalyzer.ts
@@ -1,5 +1,12 @@
 import { startServer } from '@/serve/index'
+import { environmentScanner } from '@depazer/core'
+import { errorLogger } from '@depazer/shared'
 
 export async function startAnalyzer(port: number, root: string) {
+  // 检测环境
+  const { error } = await environmentScanner(root)
+
+  if (error) return
+
   startServer(port, root)
 }
