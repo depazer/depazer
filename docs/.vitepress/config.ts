ef770b601d88ceafc9e70da0e2b38722912f490a
Jon-a-than <2140145186@qq.com>
2023-08-20 20:50:30 +0800

    docs: init home page config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,40 @@
-import {defineConfig} from 'vitepress'
+import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'Depazer',
-  description: 'A CLI tool for analyzing package dependencies',
+  description: 'JavaScript 依赖包可视化分析工具',
+  head: [['link', { rel: 'icon', href: '/logo.svg' }]],
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: { src: '/logo.svg', width: 20, height: 18 },
     nav: [
-      {text: 'Home', link: '/'},
-      {text: 'Examples', link: '/markdown-examples'}
+      { text: '指南', link: '/guide/introduction', activeMatch: '/guide/' },
+      { text: '在线体验', link: 'https://depazer.github.io/playground' }
     ],
 
     sidebar: [
       {
+        text: '开始',
+        items: [
+          { text: '简介', link: '/guide/introduction' },
+          { text: '快速开始', link: '/guide/quick-start' }
+        ]
+      },
+      {
         text: 'Examples',
         items: [
-          {text: 'Markdown Examples', link: '/markdown-examples'},
-          {text: 'Runtime API Examples', link: '/api-examples'}
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
         ]
       }
     ],
 
-    socialLinks: [
-      {icon: 'github', link: 'https://github.com/depazer/depazer'}
-    ]
+    socialLinks: [{ icon: 'github', link: 'https://github.com/depazer/depazer' }],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present Depazer'
+    }
   }
 })
---
+++ docs/guide/quick-start.md
@@ -0,0 +1,9 @@
+# 快速开始
+
+## 线上体验
+
+可以打开我们的[线上站点](https://depazer.github.io/playground)体验依赖包的可视化功能。
+
+## 安装Depazer
+
+使用你喜爱的包管理器安装`@depazer/cli`
