fc6eb68e130a02d3ffa0544fb26d50150b0e54f6
Jon-a-than <2140145186@qq.com>
2023-08-22 18:50:48 +0800

    refactor(web): move environment info card view to layout

--- packages/web/src/layouts/index.vue
+++ packages/web/src/layouts/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="tsx">
-import html2canvas from 'html2canvas'
 import GlobalSetting from '@/components/GlobalSetting.vue'
+import EnvironmentInfo from '@/components/EnvironmentInfo.vue'
 
+import html2canvas from 'html2canvas'
 import { useLocale } from '@/hooks/locale'
 
 const isDark = useDark()
@@ -30,6 +31,18 @@ const handleScreenshot = () => {
     })
   })
 }
+
+const buttonList = computed(() => [
+  {
+    title: '全屏',
+    icon: isFullscreen.value ? 'i-uil-compress' : 'i-uil-focus',
+    onClick: toggleFullscreen
+  },
+  { title: '截图', icon: 'i-ri-screenshot-fill', onClick: handleScreenshot },
+  { title: '语言', icon: 'i-uil-english-to-chinese', onClick: toggleLocale },
+  { title: '颜色模式', icon: 'i-uil-moonset dark:i-uil-sunset', onClick: toggleDark },
+  { title: '设置', icon: 'i-uil-setting', onClick: toggleSettingVisible }
+])
 </script>
 
 <template>
@@ -39,57 +52,16 @@ const handleScreenshot = () => {
     </span>
     <div>
       <button
-        title="全屏"
-        type="button"
+        bg="gray-1 hover:gray-2 active:gray-3 dark:slate-6 hover:dark:slate-7 active:dark:slate-8"
         class="mr-2 cursor-pointer rounded-full pa-1"
-        bg="gray-1 hover:gray-2 active:gray-3 dark:slate-6 hover:dark:slate-7 active:dark:slate-8"
         border="solid 1 gray-3 dark:slate-5"
-        @click="toggleFullscreen"
-      >
-        <i
-          :class="isFullscreen ? 'i-uil-compress' : 'i-uil-focus'"
-          text="2xl gray-6 dark:slate-1"
-        />
-      </button>
-      <button
-        title="语言"
+        v-for="{ title, icon, onClick } in buttonList"
+        :key="icon"
+        :title="title"
         type="button"
-        class="mr-2 cursor-pointer rounded-full pa-1"
-        bg="gray-1 hover:gray-2 active:gray-3 dark:slate-6 hover:dark:slate-7 active:dark:slate-8"
-        border="solid 1 gray-3 dark:slate-5"
-        @click="toggleLocale"
+        @click="() => onClick()"
       >
-        <i class="i-uil-english-to-chinese" text="2xl gray-6 dark:slate-1" />
-      </button>
-      <button
-        title="颜色模式"
-        type="button"
-        class="mr-2 cursor-pointer rounded-full pa-1"
-        bg="gray-1 hover:gray-2 active:gray-3 dark:slate-6 hover:dark:slate-7 active:dark:slate-8"
-        border="solid 1 gray-3 dark:slate-5"
-        @click="() => toggleDark()"
-      >
-        <i class="i-uil-moonset dark:i-uil-sunset" text="2xl gray-6 dark:slate-1" />
-      </button>
-      <button
-        title="截图"
-        type="button"
-        class="mr-2 cursor-pointer rounded-full pa-1"
-        bg="gray-1 hover:gray-2 active:gray-3 dark:slate-6 hover:dark:slate-7 active:dark:slate-8"
-        border="solid 1 gray-3 dark:slate-5"
-        @click="() => handleScreenshot()"
-      >
-        <i class="i-ri-screenshot-fill" text="2xl gray-6 dark:slate-1" />
-      </button>
-      <button
-        title="设置"
-        type="button"
-        class="cursor-pointer rounded-full pa-1"
-        bg="gray-1 hover:gray-2 active:gray-3 dark:slate-6 hover:dark:slate-7 active:dark:slate-8"
-        border="solid 1 gray-3 dark:slate-5"
-        @click="() => toggleSettingVisible()"
-      >
-        <i class="i-uil-setting" text="2xl gray-6 dark:slate-1" />
+        <i :class="icon" class="text-2xl text-gray-6 dark:text-slate-1" />
       </button>
     </div>
   </header>
@@ -103,6 +75,8 @@ const handleScreenshot = () => {
   <main ref="mainRef">
     <router-view />
   </main>
+
+  <EnvironmentInfo class="absolute right-4 bottom-4" />
 </template>
 
 <style scoped>
@@ -125,10 +99,8 @@ const handleScreenshot = () => {
     transform: translateX(0);
   }
 }
-</style>
 
-<style>
-body {
+:global(body) {
   overflow: hidden;
 }
 </style>
