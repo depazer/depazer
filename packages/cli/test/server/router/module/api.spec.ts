2b5f72e1786573ffea2152f0615e38b734678def
Jon-a-than <2140145186@qq.com>
2023-08-27 14:35:59 +0800

    refactor(cli): use @depazer/server to provide api server

--- packages/cli/test/server/router/module/api.spec.ts
+++
@@ -1,8 +0,0 @@
-import apiRoute from '@/serve/router/module/api'
-import { describe, expect, test } from 'vitest'
-
-describe('apiRoute', () => {
-  test('should be an object', () => {
-    expect(typeof apiRoute).toBe('object')
-  })
-})
--- packages/cli/test/server/router/module/base.spec.ts
+++
@@ -1,8 +0,0 @@
-import baseRoute from '@/serve/router/module/base'
-import { describe, expect, test } from 'vitest'
-
-describe('baseRoute', () => {
-  test('should be an object', () => {
-    expect(typeof baseRoute).toBe('object')
-  })
-})
