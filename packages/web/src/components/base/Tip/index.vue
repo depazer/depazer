229913995df7deea8a58abc562ea10fb19724b2a
liangzhensheng <1250564179@qq.com>
2023-09-02 19:33:39 +0800

    fix(playground): fix phone ipad display error

--- packages/web/src/components/base/Tip/index.vue
+++ packages/web/src/components/base/Tip/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="absolute top-4 w-full text-center">
+  <div id="tip" class="absolute top-4 w-90% text-center">
     depazer 的在线模式支持的功能有限，如果想要体验更多功能，请前往
     <a href="http://depazer.top" target="_blank" class="hover:color-red">depazer</a> 的官网查看 ~
   </div>
 </template>
+
+<style scoped>
+@media screen and (max-width: 1200px) {
+  #tip {
+    top: 4rem;
+  }
+}
+</style>
